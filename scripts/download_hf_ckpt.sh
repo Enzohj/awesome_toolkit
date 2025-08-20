5ea4901b9273198dc954a58d9403da1cc81f49a8
Enzohj <79735795+Enzohj@users.noreply.github.com>
2025-08-20 11:59:46 +0800

    Update download_hf_ckpt.sh

--- scripts/download_hf_ckpt.sh
+++ scripts/download_hf_ckpt.sh
@@ -1,5 +1,16 @@
 export HF_ENDPOINT=https://hf-mirror.com
 
-MODEL_NAME=$1
-# huggingface-cli download --resume-download $MODEL_NAME
-hf download $MODEL_NAME
+if [ $# -lt 1 ]; then
+    echo "用法: $0 model_name [save_dir]"
+    echo "示例1: $0 model_name save_dir  # 下载模型到指定目录"
+    echo "示例2: $0 model_name          # 下载模型到默认目录"
+    exit 1
+fi
+
+model_name=$1
+if [ $# -eq 2 ]; then
+    save_dir="$2"
+    hf download "$model_name" --local-dir "$save_dir"
+else
+    hf download "$model_name"
+fi
